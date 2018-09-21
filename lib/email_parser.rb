c7410d368c180733adc2ac4a36d29ef1f5b104ce
Rachel Wyatt <rw126@evansville.edu>
2018-09-21 11:21:00 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,9 +15,12 @@ class EmailParser
   def parse
   #  binding.pry
     email_split = @email_string.split(/,|\s/)
-    no_empty_arrays = email_split.reject {|x| x.empty?}
-    final_array = no_empty_arrays.uniq!
-    final_array
+    no_repeats = email.split.uniq?
+    
+
+  #  no_empty_arrays = email_split.reject {|x| x.empty?}
+  #  final_array = no_empty_arrays.uniq!
+  #  final_array
 
   end
 
